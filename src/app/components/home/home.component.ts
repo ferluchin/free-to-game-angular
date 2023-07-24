bad7389df44644ebcee384e629d5182ed55d05ce
Luis Granda <lgrandab@gmail.com>
2023-07-24 17:42:41 -0500

    Primera version de los servicios y home.component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+// Filename: home.component.ts
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { APIResponse, Game } from 'src/app/models';
+//import { APIResponse, Game } from '../models';
+import { HttpService } from 'src/app/services/http.service';
+//import { HttpService } from '../services/http.service';
+
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string = '';
+  public games: Array<Game> = [];
+  private routeSub!: Subscription;
+  private gameSub!: Subscription;
 
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.gameSub = this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
 }
---
+++ src/app/services/http.service.ts
@@ -0,0 +1,56 @@
+//http.service.ts
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { forkJoin, Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private http: HttpClient) {}
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http
+      //      .get(`${env.BASE_URL}/games/${id}/movies`)
+      .get(`${env.BASE_URL}/games/${id}/movies`)
+
+      .pipe(catchError((error) => of({ results: [] })));
+    const gameScreenshotsRequest = this.http
+      .get(`${env.BASE_URL}/games/${id}/screenshots`)
+      // .get(`${env.BASE_URL}/games/${id}/screenshots`)
+      .pipe(catchError((error) => of({ results: [] })));
+
+    return forkJoin({
+      gameInfoRequest,
+      gameTrailersRequest,
+      gameScreenshotsRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          trailers: resp['gameTrailersRequest']?.results,
+          screenshots: resp['gameScreenshotsRequest']?.results,
+        };
+      })
+    );
+  }
+}
