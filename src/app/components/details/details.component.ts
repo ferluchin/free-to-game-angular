95640c066310871590c352f76b74bb67a702727f
Luis Granda <lgrandab@gmail.com>
2023-07-26 20:13:49 -0500

    updated details.component.html

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/http.service';
+import { Game } from 'src/app/models';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  game: Game | undefined;
 
+  constructor(
+    private gameService: GameService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getGame();
+  }
+
+  getGame(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.gameService.getGameById(id).subscribe((game) => (this.game = game));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
