9ac09c6fa339d5f8fc0785050bc3dba94b455001
Luis Granda <lgrandab@gmail.com>
2023-07-27 16:29:49 -0500

    finished tests on details.component.spec.ts

--- src/app/components/details/details.component.spec.ts
+++ src/app/components/details/details.component.spec.ts
@@ -1,30 +1,43 @@
 // filename: details.component.spec.ts
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DetailsComponent } from './details.component';
-import { of } from 'rxjs';
+import { of, BehaviorSubject } from 'rxjs';
+import { convertToParamMap } from '@angular/router';
+
+// Define ActivatedRouteStub
+class ActivatedRouteStub {
+  private paramMapSubject = new BehaviorSubject(convertToParamMap({ id: 1 }));
+  paramMap = this.paramMapSubject.asObservable();
+
+  snapshot = {
+    paramMap: convertToParamMap({ id: 1 }),
+  };
+
+  setParamMap(params?: any) {
+    this.paramMapSubject.next(convertToParamMap(params));
+    this.snapshot.paramMap = convertToParamMap(params);
+  }
+}
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let activatedRoute: ActivatedRouteStub;
+
+  beforeEach(() => {
+    activatedRoute = new ActivatedRouteStub();
+    TestBed.configureTestingModule({
       declarations: [DetailsComponent],
       imports: [HttpClientTestingModule],
-      providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            paramMap: of(convertToParamMap({ id: 'testId' })),
-          },
-        },
-      ],
-    }).compileComponents();
-
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
+    });
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
+    // Antes de ejecutar detectChanges, seteamos los valores que queremos en el snapshot.
+    activatedRoute.setParamMap({ id: 1 });
     fixture.detectChanges();
   });
 
