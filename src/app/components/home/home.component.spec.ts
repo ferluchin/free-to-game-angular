8d43ccf5ae0fd49affe8c41e3109a31e096b87f2
Luis Granda <lgrandab@gmail.com>
2023-07-27 02:20:24 -0500

    pruebas de la aplicacion

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,21 +1,123 @@
+// home.component.spec.ts
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of, BehaviorSubject } from 'rxjs';
 import { HomeComponent } from './home.component';
+import { GameService } from './../../services/http.service'; // Corrección aquí
+import { Game } from './../../models';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+// Datos ficticios para los juegos
+const dummyGames: Game[] = [
+  {
+    id: 1,
+    title: 'Game 1',
+    thumbnail: '',
+    short_description: '',
+    genre: '',
+    platform: '',
+    publisher: '',
+    developer: '',
+    release_date: '',
+    freetogame_profile_url: '',
+    game_url: '',
+  },
+  {
+    id: 2,
+    title: 'Game 2',
+    thumbnail: '',
+    short_description: '',
+    genre: '',
+    platform: '',
+    publisher: '',
+    developer: '',
+    release_date: '',
+    freetogame_profile_url: '',
+    game_url: '',
+  },
+  // etc...
+];
+
+// Mock GameService
+class MockGameService {
+  gameFilters$ = new BehaviorSubject({
+    nombreJuego: '',
+    generoJuego: '',
+    plataformaJuego: '',
+  });
+  getGames() {
+    return of(dummyGames);
+  }
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let gameService: GameService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: GameService, useClass: MockGameService }],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    gameService = TestBed.inject(GameService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call GameService.getGames on init', () => {
+    const spy = spyOn(gameService, 'getGames').and.callThrough();
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should filter games on init', () => {
+    const spy = spyOn(component, 'filterGames');
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should change page and filter games when calling nextPage', () => {
+    const spy = spyOn(component, 'filterGames');
+    const initialPage = component.currentPage;
+
+    component.nextPage();
+
+    expect(component.currentPage).toBe(initialPage + 1);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should change page and filter games when calling previousPage and current page is greater than 0', () => {
+    component.currentPage = 1; // Asegurarse de que la página actual es mayor que 0
+    const spy = spyOn(component, 'filterGames');
+    const initialPage = component.currentPage;
+
+    component.previousPage();
+
+    expect(component.currentPage).toBe(initialPage - 1);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should not change page when calling previousPage and current page is 0', () => {
+    component.currentPage = 0; // Asegurarse de que la página actual es 0
+    const spy = spyOn(component, 'filterGames');
+
+    component.previousPage();
+
+    expect(component.currentPage).toBe(0);
+    expect(spy).not.toHaveBeenCalled();
   });
 });
--- src/app/services/http.service.spec.ts
+++ src/app/services/http.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { HttpService } from './http.service';
+import { GameService } from './http.service';
 
-describe('HttpService', () => {
-  let service: HttpService;
+describe('GameService', () => {
+  let service: GameService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GameService],
+    });
+    service = TestBed.inject(GameService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // solicitudes pendientes
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
 });
